d0d6bf61c8a69faf3db67bb2f495c00ced6ce089
innc11 <pm@innc11.cn>
2020-09-12 14:06:02 +0800

    fix: 再次尝试修复协议导致崩溃的

--- forwarder/Forwarder.py
+++ forwarder/Forwarder.py
@@ -24,14 +24,18 @@ class Forwarder(threading.Thread):
 
     def openSock(self):
 
+        if self.sock is not None:
+            self.sock.close()
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) if self.sslContext is not None else socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM, proto=0)
+        self.sock.bind((self.hostAddress, self.listenPort))
         self.sock.listen(100)
         url = ('https' if self.sslContext is not None else 'http') + '://' + self.hostAddress + ':' + str(self.listenPort)
         self.logger.info(f"Forwarder is listening on {self.listenPort}, {url}")
 
     def run(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) if self.sslContext is not None else socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM, proto=0)
-        self.sock.bind((self.hostAddress, self.listenPort))
+
 
         if self.sslContext is not None:
             while True:
